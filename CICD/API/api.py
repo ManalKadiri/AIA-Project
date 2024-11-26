f4bb1c99ae40122ce1dc08293af80c489a767e2c
ManalKadiri <manalkad@gmail.com>
2024-11-26 17:34:12 +0100

    Update api.py

--- CICD/API/api.py
+++ CICD/API/api.py
@@ -124,5 +124,5 @@ async def predict(data: List[FraudDetectionInput]):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
